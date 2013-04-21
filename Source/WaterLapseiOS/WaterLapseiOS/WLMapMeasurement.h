3474cc20a5332395071427408475bfc3e693705f
Hector Zarate <hecktorzr@gmail.com>
2013-04-21 11:44:10 +0200

    Added class for measurements overlay in the map

---
+++ Source/WaterLapseiOS/WaterLapseiOS/WLMapMeasurement.h
@@ -0,0 +1,16 @@
+//
+//  WLMapMeasurement.h
+//  WaterLapseiOS
+//
+//  Created by Hector Zarate on 4/21/13.
+//  Copyright (c) 2013 International Space Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface WLMapMeasurement : NSObject <MKOverlay>
+
+
+
+@end
---
+++ Source/WaterLapseiOS/WaterLapseiOS/WLMapMeasurement.m
@@ -0,0 +1,13 @@
+//
+//  WLMapMeasurement.m
+//  WaterLapseiOS
+//
+//  Created by Hector Zarate on 4/21/13.
+//  Copyright (c) 2013 International Space Apps. All rights reserved.
+//
+
+#import "WLMapMeasurement.h"
+
+@implementation WLMapMeasurement
+
+@end
