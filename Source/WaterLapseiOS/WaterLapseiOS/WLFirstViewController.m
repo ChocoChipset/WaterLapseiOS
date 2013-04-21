46c6884e49d2ca7dfad5caf294401041660d5e33
Hector Zarate <hecktorzr@gmail.com>
2013-04-21 15:50:54 +0200

    Added an example of MKOverlay

--- Source/WaterLapseiOS/WaterLapseiOS/WLFirstViewController.m
+++ Source/WaterLapseiOS/WaterLapseiOS/WLFirstViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "WLFirstViewController.h"
+#import <MapKit/MapKit.h>
+#import "WLMeasurementOverlay.h"
+#import "WLMapMeasurement.h"
+#import "WLMapMeasurement.h"
 
 @interface WLFirstViewController ()
 
@@ -14,16 +18,54 @@
 
 @implementation WLFirstViewController
 
+
+/*--------------------------------------------------------*/
+
+#pragma mark - View Loading
+
+/*--------------------------------------------------------*/
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    WLMapMeasurement *exampleOverlay = [[WLMapMeasurement alloc] initWithCoordinate:kExampleOverlayCoordinate withMeasurementValue:1.0];
+    
+    MKCoordinateSpan centerSpan = MKCoordinateSpanMake(0.005, 0.005);
+    
+    MKCoordinateRegion centerRegion = MKCoordinateRegionMake(kExampleOverlayCoordinate, centerSpan);
+    [self.mapView addOverlay:exampleOverlay];
+    
+    [self.mapView setRegion:centerRegion animated:YES];
 }
+
+/*--------------------------------------------------------*/
+
+#pragma mark - Memory Warning 
+
+/*--------------------------------------------------------*/
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*--------------------------------------------------------*/
+
+#pragma mark - MKMapView Delegate
+
+/*--------------------------------------------------------*/
+
+- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id<MKOverlay>)overlay
+{
+    WLMapMeasurement *measurementOverlay = (WLMapMeasurement *)overlay;
+    
+    WLMeasurementOverlay *mapOverlayView = [[WLMeasurementOverlay alloc] initWithOverlay:measurementOverlay];
+    
+    return mapOverlayView;
+}
+
+/*--------------------------------------------------------*/
 
 @end
